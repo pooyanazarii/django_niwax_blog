8f54eeab5a6deab35217263f07c07d56eec78832
Pooya Nazari <66327968+pooyanazarii@users.noreply.github.com>
2022-08-24 14:53:44 +0430

    added simple templates
    added root templates in settings
    added url and root static in settings
    added url and root media in settings
    added folders

--- bniwax_pj/settings.py
+++ bniwax_pj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6snfu*%89j#y91jf^@cy^*_1d+7*8l33&dfcr5cd7v5c7%l42m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #this is added with me
+    'iblog.apps.IblogConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'bniwax_pj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR /'static'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+STATICFILES_DIRS  = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
