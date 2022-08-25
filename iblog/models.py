2f14d049f977b5135fe5bab0623c899650d7fe66
Pooya Nazari <66327968+pooyanazarii@users.noreply.github.com>
2022-08-25 12:37:05 +0430

    added models in models file in app

--- iblog/models.py
+++ iblog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    text_content = models.TextField()
+    author = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    #tag
+    counted_view = models.IntegerField()
+    status = models.BooleanField()
+    published_date = models.DateTimeField()
+    created_date = models.DateTimeField()
+    updated_date = models.DateTimeField()
+
+
