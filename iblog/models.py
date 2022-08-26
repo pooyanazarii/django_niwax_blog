5921659df3876456d3ac7f99f9d387f57082a4b3
Pooya Nazari <66327968+pooyanazarii@users.noreply.github.com>
2022-08-26 19:49:37 +0430

    added table 'author', 'tag', 'image'
    added models
    added admin orm part
    added templates tag in html file
    created folder media
    set default for image

--- iblog/models.py
+++ iblog/models.py
@@ -1,15 +1,22 @@
+from distutils.command.upload import upload
 from turtle import title
 from django.db import models
 from django.utils.html import format_html
 from django.contrib import admin
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Tags(models.Model):
+    nametag = models.CharField(max_length=255)
+    def __str__(self):
+        return self.nametag
 class Post(models.Model):
+    tags = models.ManyToManyField(Tags)
+    image = models.ImageField(upload_to ='blog/',default = 'blog/default.jpg')
     title = models.CharField(max_length=255)
     text_content = models.TextField()
-    author = models.CharField(max_length=255)
+    author = models.ForeignKey(User,on_delete=models.SET_NULL, null= True)
     category = models.CharField(max_length=255)
-    #tag
     counted_view = models.IntegerField()
     status = models.BooleanField()
     published_date = models.DateTimeField()
