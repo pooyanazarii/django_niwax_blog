b68d6b1872bda037415c04c658479f65dcd4e9ee
Pooya Nazari <66327968+pooyanazarii@users.noreply.github.com>
2022-08-27 12:10:12 +0430

    get title of post with template tag

--- iblog/templatetags/blog_tag.py
+++ iblog/templatetags/blog_tag.py
@@ -1,7 +1,11 @@
 from django import template
-
+from iblog.models import Post
 register = template.Library()
 
 @register.simple_tag
 def funplus (a):
     return a+2
+
+@register.simple_tag(name='totlaposts')
+def allpost ():
+    return Post.objects.filter(status=1)
