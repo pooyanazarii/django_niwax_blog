5c76f7647f214a43ad69f2fc29050a0f26d4f7d5
Pooya Nazari <66327968+pooyanazarii@users.noreply.github.com>
2022-08-30 22:39:40 +0430

    added contact model to website app and delete from iblog

--- website/models.py
+++ website/models.py
@@ -1,11 +1,13 @@
+
 from django.db import models
 
 # Create your models here.
-# class Contact(models.Model):
-#     name = models.CharField(max_length=255)
-#     subject = models.CharFiled(max_length=255)
-#     email = models.CharField(max_length=255)
-#     message = models.TextField()
-#     create_date = models.DateTimeField(auto_now_add=True)
-#     updated_date = models.DateTimeField(auto_now=True)
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    subject = models.CharField(max_length=255,null = True)
+    email = models.CharField(max_length=255)
+    number = models.CharField(max_length=255)
+    message = models.TextField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
 
