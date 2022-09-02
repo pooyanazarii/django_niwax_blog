f007a3f1149c802ebd7d9c71214ae30a83b321f7
Pooya Nazari <66327968+pooyanazarii@users.noreply.github.com>
2022-09-02 12:41:43 +0430

    Added robots.txt file

--- bniwax_pj/settings.py
+++ bniwax_pj/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #robot
+    'robots',
     # for Modules
     'django.contrib.humanize',
     #site framework
@@ -124,6 +126,8 @@ USE_L10N = True
 USE_TZ = True
 
 SITE_ID = 2
+ROBOTS_USE_HOST = True
+ROBOTS_USE_SITEMAP = True
 
 
 # Static files (CSS, JavaScript, Images)
--- bniwax_pj/urls.py
+++ bniwax_pj/urls.py
@@ -34,6 +34,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
         path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
          name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt', include('robots.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
