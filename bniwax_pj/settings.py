529d8bbcb7901d1507e501752abe63c234ce1f0f
Pooya Nazari <66327968+pooyanazarii@users.noreply.github.com>
2022-09-13 20:21:40 +0430

    Preparing the site for upload

--- bniwax_pj/settings.py
+++ bniwax_pj/settings.py
@@ -16,16 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6snfu*%89j#y91jf^@cy^*_1d+7*8l33&dfcr5cd7v5c7%l42m'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -104,15 +95,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'bniwax_pj.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -145,8 +127,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-#site map config
-SITE_ID = 2
+
 # robots.txt config
 ROBOTS_USE_HOST = True
 ROBOTS_USE_SITEMAP = True
@@ -188,12 +169,9 @@ SUMMERNOTE_CONFIG = {
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR /'static'
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR /'media'
-STATICFILES_DIRS  = [
-    BASE_DIR / 'static',
-]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
