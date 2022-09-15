2bebf0138bf28146f87a3ca2232618abd28e84f0
Pooya Nazari <66327968+pooyanazarii@users.noreply.github.com>
2022-09-15 23:55:27 +0430

    Update manage.py

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bniwax_pj.setting.prod')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bniwax_pj.setting.dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
