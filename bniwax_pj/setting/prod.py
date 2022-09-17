7938aac99047fdc0c95e21546b8f12847ad8b3e8
Pooya Nazari <66327968+pooyanazarii@users.noreply.github.com>
2022-09-17 20:47:45 +0430

    security update

--- bniwax_pj/setting/prod.py
+++ bniwax_pj/setting/prod.py
@@ -36,4 +36,19 @@ COMPRESS_ROOT = BASE_DIR /'static_comp'
 COMPRESS_ENABLED =True
 
 
-# CSRF_COOKIE_SECURE = True
+## X-XSS-Protection
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+## Strict-Transport-Security
+SECURE_HSTS_SECONDS = 15768000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+## that requests over HTTP are redirected to HTTPS. aslo can config in webserver
+SECURE_SSL_REDIRECT = True 
+
+# for more security
+CSRF_COOKIE_SECURE = True
+CSRF_USE_SESSIONS = True
+CSRF_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SECURE = True
